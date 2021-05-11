a0830308f13228bd5affe021f16c9c105d8d7617
soojubm <soojubm@gmail.com>
2021-05-11 18:21:49 +0900

    refector: script

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -26,59 +26,67 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
 
+  // todo functoin
+  const TAB_FIXED_CLASSNAME = 'is-fixed-home-tablist'
   const stickyElement = document.querySelector('.js-home-tablist')
   const navbarElement = document.querySelector('.js-navbar-backdrop')
   const heroElement = document.querySelector('.js-hero')
+  // const { bodyElement } = document
 
   let previousOffsetTop
   let isStuck
-  let currentScrollPostion
+  let currentScrollTop
+  
+  const getWindowOffsetTop = () => window.scrollY || window.pageYOffset
 
   window.addEventListener('load', () => {
     previousOffsetTop = stickyElement.offsetTop;
-    currentScrollPostion = window.scrollY || window.pageYOffset
+    currentScrollTop = getWindowOffsetTop()
   })
-
-  // resize시 navbarHeight + heroHeight 되어야 함. offsetTop은 sticky 상태에 따라 유동
   window.addEventListener('resize', throttle(setElementOffsetTop), false)
+  window.addEventListener('scroll', throttle(handleElementScroll), false)
+
   function setElementOffsetTop() {
-    currentScrollPostion = window.scrollY || window.pageYOffset
-    isStuck = currentScrollPostion > previousOffsetTop
+    currentScrollTop = getWindowOffsetTop()
+    isStuck = currentScrollTop > previousOffsetTop
     previousOffsetTop = navbarElement.clientHeight + heroElement.clientHeight
   }
 
-  window.addEventListener('scroll', throttle(handleElementScroll), false)
   function handleElementScroll() {
-    currentScrollPostion = window.scrollY || window.pageYOffset
-    isStuck = currentScrollPostion > previousOffsetTop
+    currentScrollTop = getWindowOffsetTop()
+    isStuck = currentScrollTop > previousOffsetTop
     
-    document.body.classList.toggle('is-fixed-home-tablist', isStuck)
+    document.body.classList.toggle(TAB_FIXED_CLASSNAME, isStuck)
   }
 
 
   let lastScrollTop = 0;
-  window.addEventListener("scroll", () => {
-    var currentScrollTop = window.pageYOffset || document.documentElement.scrollTop
+  window.addEventListener("scroll", throttle(handleNavbar), false);
+
+  function handleNavbar() {
+    const currentScrollTop = getWindowOffsetTop()
     const isScrolledDown = currentScrollTop > lastScrollTop
-    const isFixedTab = document.body.classList.contains('is-fixed-home-tablist')
+    const isFixedTab = document.body.classList.contains(TAB_FIXED_CLASSNAME)
 
-    if(isFixedTab) {
-
-    }
     document.body.classList.toggle('is-navbar-up', isFixedTab && isScrolledDown)
 
     lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // For Mobile or negative scrolling
-  }, false);
+  }
 
+
+
+
+
+
+  
 
   const ANIMATED_CLASSNAME = 'is-observed'
   const elements = Array.from(document.querySelectorAll('.js-observer'))
-    // console.log([].slice.call(document.querySelectorAll('.js-scroll-animation')), Array.from(document.querySelectorAll('.js-scroll-animation')),  document.querySelectorAll('.js-scroll-animation'))
-    const options = {
-      root: null,
-      rootMargin: '0px 0px 0px 0px',
-      threshold: 0.25
-    }
+  const options = {
+    root: null,
+    rootMargin: '0px 0px 0px 0px',
+    threshold: 0.5
+  }
   let observer = new IntersectionObserver(callback, options)
 
   elements.forEach(element => observer.observe(element))
@@ -87,23 +95,20 @@ document.addEventListener('DOMContentLoaded', () => {
     entries.forEach(entry => {
       if (!entry.isIntersecting) return
       entry.target.classList.toggle(ANIMATED_CLASSNAME, entry.isIntersecting)
-
-      // entry.target.classList.add('pulse')
-      // entry.target.addEventListener('animationend', event => event.currentTarget.classList.remove(ANIMATED_CLASSNAME));
-      // observer.unobserve(entry.target) // once
-      // if(entry.intersectionRatio > 0) {}
     })
   }
 
 
-
-  // 
+  // todo mobile;;;
   const homeCTA = document.querySelector('.js-home-cta')
   if(homeCTA) {
     window.addEventListener('scroll', throttle(test12))
   }
   function test12() {
-    const isStuck = window.pageYOffset > 1080
+    const heroElement = document.querySelector('.js-hero')
+    if(!heroElement) return
+
+    const isStuck = window.pageYOffset > heroElement.clientHeight
     homeCTA.classList.toggle('is-fixed', isStuck)
   }
 })
