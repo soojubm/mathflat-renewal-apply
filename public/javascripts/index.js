2a5e193d314bc7ace92220ebc446a3cf37dc02aa
soojubm <soojubm@gmail.com>
2021-05-13 14:02:51 +0900

    feature: get selected tab when refresh

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,10 +2,8 @@ import { throttle } from './utils/optimizationUtils.js'
 
 document.addEventListener('DOMContentLoaded', () => {
 
+
   let lastScrollTop = 0;
-
-
-
 
   const tab = document.querySelector('.js-tab')
   if(!tab) return
@@ -13,8 +11,20 @@ document.addEventListener('DOMContentLoaded', () => {
   const tablist = tab.querySelectorAll('[role=tab]')
   const tabPanels = tab.querySelectorAll('[role=tabpanel]')
 
+  let { hash } = window.location
+  const lastTab = Number(hash.substring(4, 5)) || 0
+
+  // 이전 탭 저장 id hash로..
+  tablist.forEach((tab, tabIndex) => tab.setAttribute('aria-selected', lastTab === tabIndex + 1))
+  tabPanels.forEach((tabPanel, tabPanelIndex) => {
+    const isTargetPanel = lastTab === tabPanelIndex + 1
+    console.log(lastTab, tabPanelIndex)
+    tabPanel.setAttribute('aria-hidden', String(!isTargetPanel))
+  })
+
   tablist.forEach((tab, tabIndex) => {
-    tab.addEventListener('click', () => {
+    tab.addEventListener('click', event => {
+      // event.preventDefault()
 
       tablist.forEach(tab => tab.setAttribute('aria-selected', 'false'))
       tab.setAttribute('aria-selected', 'true')
@@ -26,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       // + 100 임시
       // 여기서 이전 스크롤 값을 넣어줘야 한다......><
-      window.scrollTo(0, navbarElement.clientHeight + heroElement.clientHeight + 100)
+      window.scrollTo(0, navbarElement.clientHeight + heroElement.clientHeight + 20)
       // lastScrollTop = 0;
       document.body.classList.add('is-navbar-up')
 
@@ -119,58 +129,64 @@ document.addEventListener('DOMContentLoaded', () => {
     homeCTA.classList.toggle('is-fixed', isStuck)
   }
 
-  let startNr = 0
+  const counters = document.querySelectorAll('.js-counter')
+  let starttime
 
-  // const counters = document.querySelectorAll('.js-counter')
+  counters.forEach(counter => {
+    const limitNumber = parseInt(counter.textContent)
 
-  // window.addEventListener('scroll', () => {
-  //   counters.forEach(counter => {
-  //     const isTemp = counter.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.classList.contains('is-observed')
-  //     if(isTemp) {
-  //       counter.innerText = startNr
-  //       const endNumber = parseInt(counter.textContent)
-  //       if(startNr < endNumber) {
-  //         setTimeout(() => startNr + 1, 100)
-  //       }
-  //       return 
-  //     }
-  //   })
-  // })
+    window.addEventListener('scroll', () => {
+      if(counter.classList.contains('is-play')) return
 
+      let number = 0;
 
-  const videoElements = document.querySelectorAll('video')
+      const isTemp = counter.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.classList.contains('is-observed')
+      if(!isTemp) return
+      setTimeout(() => {
+        counter.classList.add('is-play')
 
-  window.addEventListener('scroll', () => {
-    videoElements.forEach(videoElement => {
-      console.log(videoElement.parentNode.parentNode.parentNode)
-      if(videoElement.parentNode.parentNode.parentNode.classList.contains('is-observed')) {
-        videoElement.play()
+        requestAnimationFrame(function(timestamp) {
+          starttime = timestamp || new Date().getTime() //if browser doesn't support requestAnimationFrame, generate our own timestamp using Date
+          moveit(timestamp, counter, 1600)
+        })
+      }, 1200)
+
+      function moveit(timestamp, el, duration){
+        //if browser doesn't support requestAnimationFrame, generate our own timestamp using Date:
+        var timestamp = timestamp || new Date().getTime()
+        var runtime = timestamp - starttime
+        var progress = runtime / duration
+        progress = Math.min(progress, 1)
+
+        // el.style.left = (400 * progress).toFixed(2) + 'px'
+
+        if(number >= limitNumber) return
+        if(counter.classList.contains('is-play')) return
+
+        number++
+        counter.innerText = `${number}%`
+
+        if (runtime < duration){ // if duration not met yet
+          requestAnimationFrame(timestamp => { // call requestAnimationFrame again with parameters
+            moveit(timestamp, el, duration)
+          })
+        }
       }
     })
   })
 
 
-  window.addEventListener('scroll', throttle(handleElementScroll), false)
+  // const videoElements = document.querySelectorAll('video')
 
-  function detectBrowser() {
-    const browserElement = document.querySelector('.js-browser')
-    const wrapElement = document.querySelector('.js-wrap')
-    if (!browserElement || !wrapElement) return
-  
-    const userAgent = navigator.userAgent.toLowerCase()
-    const isIEBrowser = userAgent.indexOf('msie') > -1 || userAgent.indexOf('trident') > -1
-    // const isChromeBrowser = userAgent.indexOf('chrome') !== -1
-    // const isMacBrowser = /Mac|iPod|iPhone|iPad/.test(navigator.platform);
-  
-    browserElement.hidden = !isIEBrowser
-    // wrapElement.hidden = isIEBrowser
-  
-    if(isIEBrowser) wrapElement.style.display = 'none'
-  
-    console.log(isIEBrowser)
-  
-    // todo return browser
-  }
+  // window.addEventListener('scroll', () => {
+  //   videoElements.forEach(videoElement => {
+  //     console.log(videoElement.parentNode.parentNode.parentNode)
+  //     if(videoElement.parentNode.parentNode.parentNode.classList.contains('is-observed')) {
+  //       videoElement.play()
+  //     }
+  //   })
+  // })
+
   function detectBrowser() {
     const browserElement = document.querySelector('.js-browser')
     const wrapElement = document.querySelector('.js-wrap')
