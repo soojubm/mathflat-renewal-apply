9e4039c0a0371939133e71b9c63a74e288d6030c
soojubm <soojubm@gmail.com>
2021-05-18 11:49:28 +0900

    fa

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -103,24 +103,6 @@ document.addEventListener('DOMContentLoaded', () => {
     // }
   }
 
-  const ANIMATED_CLASSNAME = 'is-observed'
-  const elements = Array.from(document.querySelectorAll('.js-observer'))
-  const options = {
-    root: null,
-    rootMargin: '0px 0px 0px 0px',
-    threshold: 0.25,
-  }
-  let observer = new IntersectionObserver(callback, options)
-
-  elements.forEach(element => observer.observe(element))
-
-  function callback(entries, observer) {
-    entries.forEach(entry => {
-      if (!entry.isIntersecting) return
-      entry.target.classList.toggle(ANIMATED_CLASSNAME, entry.isIntersecting)
-    })
-  }
-
   const videoElements = document.querySelectorAll('video')
   if (videoElements) {
     videoElements.forEach(videoElement => {
@@ -186,17 +168,5 @@ document.addEventListener('DOMContentLoaded', () => {
         // }, 200)
       }
     }
-  }
-
-  const homeCTA = document.querySelector('.js-home-cta')
-  if (homeCTA) {
-    window.addEventListener('scroll', throttle(test12))
-  }
-  function test12() {
-    const heroElement = document.querySelector('.js-hero-cta')
-    if (!heroElement) return
-
-    const isStuck = window.pageYOffset > heroElement.offsetTop
-    homeCTA.classList.toggle('is-fixed', isStuck)
   }
 })
